7af3578aa2cdcb6353a8829694dbf88e760a7c34
chino-hdp <victort8012@gmail.com>
2023-01-11 18:43:38 -0300

    Creando funcion para dividir los herores segun editorial

---
+++ src/heroes/helpers/getHeroesByPublisher.js
@@ -0,0 +1,11 @@
+import { heroesData } from "../data";
+
+export const getHeroesByPublisher = (publisher) => {
+  const validPublishers = ["DC Comics", "Marvel Comics"];
+
+  if (!validPublishers.includes(publisher)) {
+    throw new Error(`Publisher "${publisher}" no es correcto`);
+  }
+
+  return heroesData.filter( hero => hero.publisher === publisher );
+};

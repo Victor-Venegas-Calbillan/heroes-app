91ec80a857c236852a2cb3a883b27194fef7ceaf
Victor-Venegas-Calbillan <victort8012@gmail.com>
2023-01-12 16:36:14 -0300

    Creando customHook useForm

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,39 @@
+import { HeroCard } from "../components";
+
 export const SearchPage = () => {
   return (
     <>
-      <h1>SearchPage</h1>
+      <h1 className="animate__animated animate__fadeIn">SearchPage</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5 animate__animated animate__fadeInLeft">
+          <h3>Search</h3>
+          <hr />
+
+          <form className="mt-3">
+            <input
+              type="text"
+              placeholder="Find your hero"
+              className="form-control"
+              autoComplete="off"
+              name="searchText"
+            />
+
+            <button className="btn btn-outline-primary mt-3">
+              search
+            </button>
+          </form>
+        </div>
+        <div className="col-7 animate__animated animate__fadeInRight">
+          <h3>Resultados</h3>
+          <hr />
+          <div className="alert alert-primary">Search a hero</div>
+          <div className="alert alert-danger">No hero with <b>ABC</b></div>
+
+          {/* <HeroCard /> */}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};

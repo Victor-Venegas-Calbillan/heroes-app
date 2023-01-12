ca907f60cd7f5fd6495737d17a6605d726d745b8
Victor-Venegas-Calbillan <victort8012@gmail.com>
2023-01-12 13:35:47 -0300

    Recibir id del heroe por parametros

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+  const { heroId } = useParams();
+
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <>
-      <h1>HeroPage</h1>
+      
     </>
   )
 }
